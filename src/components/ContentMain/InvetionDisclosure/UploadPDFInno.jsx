298016de3ed104c7534280e6cb7a3e4c19c4519a
Developeraltbig <developeraltbig@gmail.com>
2024-11-12 15:43:34 +0530

    Update UploadPDFInno.jsx

--- src/components/ContentMain/InvetionDisclosure/UploadPDFInno.jsx
+++ src/components/ContentMain/InvetionDisclosure/UploadPDFInno.jsx
@@ -527,7 +527,7 @@ function UploadPDFInno() {
     setLoading(true); // Start loading
     try {
       const response = await axios.post(
-        "http://ino360-backend-26.onrender.com/upload",
+        "https://ino360-backend-26.onrender.com/upload",
         formData
       );
       alert(response.data.message);
