b1bc5e3a9c7f07e8c7e9fc447c2b5106136bfad3
Developeraltbig <developeraltbig@gmail.com>
2024-11-26 15:06:50 +0530

    Update ProvisionalDrafting-new.jsx

--- src/components/ContentMain/ProvisionalDrafting/ProvisionalDrafting-new.jsx
+++ src/components/ContentMain/ProvisionalDrafting/ProvisionalDrafting-new.jsx
@@ -114,7 +114,7 @@ const ProvisionalDraftingnew = () => {
     setAnswer6("Generating Answer... Wait for a while...");
     try {
       const response = await axios({
-        url: `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=AIzaSyAUrKHvLgqbCbmWzdQUBGUTcNQq35HuXRQ`,
+        url: `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=AIzaSyAJ6hLxr058t7H7bI41aL0PNYKsLYCkOVs`,
         method: "post",
         data: {
           contents: [{ parts: [{ text: question6 }] }],
